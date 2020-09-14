58bc2d6354c515972c147581c826e752d0f04327
Shivam Bansal <shivambansal157@gmail.com>
2020-09-14 23:09:16 +0530

    react setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build a Covid 19 Tracker software_artist!!!</h1>
+    </div>
+  );
+}
+
+export default App;
