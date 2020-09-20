3820b58ef90ac90065301a6c80ee366a759f60dc
Shivam Bansal <shivambansal157@gmail.com>
2020-09-20 16:51:38 +0530

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA', 'UK', 'INDIA'
+  ]);
   return (
     <div className="app">
-      <h1>Lets build a Covid 19 Tracker software_artist!!!</h1>
+      <div className="app__header">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value='abc'
+          >
+            {/* Loop through all the countries and show a drop down list of the options */}
+
+            {
+              countries.map(country => (
+                <MenuItem value="{country}">{country}</MenuItem>
+
+              ))
+            }
+            
+            {/*<MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Sachin</MenuItem>
+            <MenuItem value="worldwide">Dhoni</MenuItem>
+  <MenuItem value="worldwide">Mischel</MenuItem>*/}
+
+          </Select>
+      </FormControl>
+      </div>
+      
     </div>
   );
 }
