8b9bc076051988bf5a4935942bcf21e5267bc99a
Shivam Bansal <shivambansal157@gmail.com>
2020-09-22 23:30:31 +0530

    How to fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect  } from 'react';
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 import './App.css';
 
 function App() {
+  //STATE = How to write a variable in REACT<<< 
   const [countries, setCountries] = useState([
     'USA', 'UK', 'INDIA'
   ]);
+
+  // https://disease.sh/v3/covid-19/countries
+
+  //USEEFFECT = Runs a piece of code based on a given condition
+
+  useEffect(() => {
+    // The code inside here will run once when the component loads and not again
+    // async -> send a request, wait for it, do something with info
+  },[]); 
+
   return (
     <div className="app">
       <div className="app__header">
